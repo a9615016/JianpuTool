caf4ce63ab33af462df9805aafb1994c8ec30c88
a9615016 <a9615016@gmail.com>
2026-07-23 17:35:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,111 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+import os
+import uuid
+
+from music21 import converter
+
+
+app = FastAPI()
+
+
+UPLOAD_DIR = "uploads"
+OUTPUT_DIR = "outputs"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+
 @app.get("/", response_class=HTMLResponse)
 def home():
 
     return """
-    <!DOCTYPE html>
-    <html>
+    <h1>🎵 JianpuTool MIDI → 簡譜</h1>
 
-    <head>
-        <meta charset="utf-8">
-        <title>JianpuTool</title>
-    </head>
+    <form action="/midi" method="post" enctype="multipart/form-data">
 
+        <input type="file" name="file" accept=".mid">
 
-    <body>
+        <br><br>
 
-        <h1>🎵 JianpuTool</h1>
+        <button>
+        產生簡譜 PDF
+        </button>
 
-        <h2>MIDI → 數字簡譜 PDF</h2>
-
-
-        <form action="/midi" method="post" enctype="multipart/form-data">
-
-            <input 
-                type="file" 
-                name="file"
-                accept=".mid,.midi"
-            >
-
-            <br><br>
+    </form>
+    """
 
 
-            <button type="submit">
 
-                產生簡譜 PDF
+@app.get("/status")
+def status():
 
-            </button>
+    return {
+        "status": "JianpuTool running",
+        "api": [
+            "/midi"
+        ]
+    }
 
 
-        </form>
+
+@app.post("/midi")
+async def midi(file: UploadFile = File(...)):
+
+    try:
+
+        filename = str(uuid.uuid4()) + ".mid"
+
+        midi_path = os.path.join(
+            UPLOAD_DIR,
+            filename
+        )
 
 
-    </body>
+        with open(midi_path, "wb") as f:
 
-    </html>
+            f.write(await file.read())
+
+
+
+        score = converter.parse(
+            midi_path
+        )
+
+
+        xml_name = filename.replace(
+            ".mid",
+            ".musicxml"
+        )
+
+
+        xml_path = os.path.join(
+            OUTPUT_DIR,
+            xml_name
+        )
+
+
+        score.write(
+            "musicxml",
+            fp=xml_path
+        )
+
+
+        return {
+
+            "status": "success",
+
+            "musicxml": xml_name
+
+        }
+
+
+    except Exception as e:
+
+        return {
+
+            "error": str(e)
+
+        }
     
