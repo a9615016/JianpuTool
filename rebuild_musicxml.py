3ece490c6a3a3b137a596286aca1c76e9949ad3c
a9615016 <a9615016@gmail.com>
2026-07-24 15:58:02 +0800

    Update rebuild_musicxml.py

--- rebuild_musicxml.py
+++ rebuild_musicxml.py
@@ -1,44 +1,145 @@
 import sys
-from music21 import converter, stream, meter
+from music21 import converter, meter, stream
 
 
-print("REBUILD VERSION 20260724 V4")
+print("REBUILD VERSION 20260724 V5")
 
 
 def rebuild(input_file, output_file):
 
     print("開始重建 MusicXML")
 
-    score = converter.parse(input_file)
+
+    score = converter.parse(
+        input_file
+    )
 
 
     for part in score.parts:
 
-        # 強制 4/4
+
+        # 移除原本怪拍號
+        for ts in part.recurse().getElementsByClass(
+            meter.TimeSignature
+        ):
+            ts.activeSite.remove(ts)
+
+
+        # 加入4/4
         part.insert(
             0,
             meter.TimeSignature("4/4")
         )
 
 
-        for measure in part.getElementsByClass(
-            stream.Measure
-        ):
-
-            # 修正怪 duration
-            for element in measure.notesAndRests:
-
-                ql = element.duration.quarterLength
+        # 重新建立 measure
+        measures = []
 
 
-                # 不合法長度修正
-                if ql > 4:
+        current = stream.Measure(
+            number=1
+        )
 
-                    element.duration.quarterLength = 4
+
+        total = 0
 
 
-                # 強制重新計算 type
-                element.duration.linked = True
+        for n in part.flat.notesAndRests:
+
+
+            ql = n.duration.quarterLength
+
+
+            # 修正非法長度
+            if ql <= 0:
+                continue
+
+
+            # 超過一拍拆掉
+            while ql > 4:
+
+                n.duration.quarterLength = 4
+                ql = 4
+
+
+            # 小節滿了換下一小節
+
+            if total + ql > 4:
+
+
+                measures.append(
+                    current
+                )
+
+
+                current = stream.Measure(
+                    number=len(measures)+1
+                )
+
+
+                total = 0
+
+
+
+            current.append(
+                n
+            )
+
+
+            total += ql
+
+
+
+            if total == 4:
+
+
+                measures.append(
+                    current
+                )
+
+
+                current = stream.Measure(
+                    number=len(measures)+1
+                )
+
+
+                total = 0
+
+
+
+        if len(current.notesAndRests) > 0:
+
+            measures.append(
+                current
+            )
+
+
+        new_part = stream.Part()
+
+
+        new_part.insert(
+            0,
+            meter.TimeSignature("4/4")
+        )
+
+
+        for m in measures:
+
+            new_part.append(
+                m
+            )
+
+
+
+        score.remove(
+            part
+        )
+
+
+        score.insert(
+            0,
+            new_part
+        )
 
 
 
