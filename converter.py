8e9cf8e86642bf27be0870f0f5ee94699a022d84
a9615016 <a9615016@gmail.com>
2026-07-24 17:47:51 +0800

    Update converter.py

--- converter.py
+++ converter.py
@@ -7,9 +7,7 @@ def midi_to_musicxml(input_file):
 
     print("開始 MIDI → MusicXML")
 
-
     folder=os.path.dirname(input_file)
-
 
     output_file=os.path.join(
         folder,
@@ -17,40 +15,67 @@ def midi_to_musicxml(input_file):
     )
 
 
-    score=music21.converter.parse(
-        input_file
-    )
+    score=music21.converter.parse(input_file)
+
+
+    print("保留第一聲部")
+
+    parts=score.parts
+
+    if len(parts)>0:
+        score=parts[0]
 
 
     print("量化")
 
-
     score.quantize(
-        quarterLengthDivisors=[
-            4,
-            8,
-            16
-        ]
+        quarterLengthDivisors=[4,8,16]
     )
+
+
+    print("強制4/4")
+
+
+    # 清除舊拍號
+    for ts in score.recurse().getElementsByClass(
+        music21.meter.TimeSignature
+    ):
+        ts.ratioString="4/4"
+
+
+    if len(
+        score.recurse().getElementsByClass(
+            music21.meter.TimeSignature
+        )
+    )==0:
+
+        score.insert(
+            0,
+            music21.meter.TimeSignature("4/4")
+        )
 
 
 
     print("建立小節")
 
 
-    # 強制 4/4
-
-    score.insert(
-        0,
-        music21.meter.TimeSignature("4/4")
+    score.makeMeasures(
+        inPlace=True
     )
 
 
+    print("修正小節")
 
-    # 移除多餘譜表
 
-    score=score.flatten()
+    for m in score.recurse().getElementsByClass(
+        music21.stream.Measure
+    ):
 
+        m.timeSignature = music21.meter.TimeSignature("4/4")
+
+
+
+    print("輸出")
 
 
     score.write(
@@ -59,27 +84,15 @@ def midi_to_musicxml(input_file):
     )
 
 
-
-    print(
-        "MusicXML完成"
-    )
-
     print(output_file)
-
 
 
     return output_file
 
 
 
-
-
 if __name__=="__main__":
-
 
     midi=sys.argv[1]
 
-
-    midi_to_musicxml(
-        midi
-    )
+    midi_to_musicxml(midi)
