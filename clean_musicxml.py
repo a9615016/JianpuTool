cd18b35c51915c71ab8169a9ce0c51271ac59979
a9615016 <a9615016@gmail.com>
2026-07-24 11:53:42 +0800

    Update clean_musicxml.py

--- clean_musicxml.py
+++ clean_musicxml.py
@@ -10,7 +10,8 @@ def clean_musicxml(input_file, output_file):
     root = tree.getroot()
 
 
-    # 修正所有 note
+    # 修正 note pitch
+
     for note in root.findall(".//note"):
 
         pitch = note.find("pitch")
@@ -23,7 +24,6 @@ def clean_musicxml(input_file, output_file):
         octave = pitch.find("octave")
 
 
-        # 修正 step
         if step is not None:
 
             if step.text not in [
@@ -35,69 +35,72 @@ def clean_musicxml(input_file, output_file):
                 "F",
                 "G"
             ]:
+
                 step.text = "C"
 
 
 
-        # 修正 octave
         if octave is not None:
 
             try:
-                value = int(octave.text)
+                value=int(octave.text)
 
             except:
-                value = 4
+                value=4
 
 
-            if value < 1:
-                value = 4
+            if value < 2:
+                value=4
+
+            if value > 7:
+                value=4
 
 
-            if value > 8:
-                value = 4
+            octave.text=str(value)
 
 
-            octave.text = str(value)
+
+        # 移除異常 pitch alter
+
+        alter = pitch.find("alter")
+
+        if alter is not None:
+
+            try:
+                a=float(alter.text)
+
+                if a not in [-2,-1,0,1,2]:
+                    pitch.remove(alter)
+
+            except:
+
+                pitch.remove(alter)
+
+
+
+    # 修正 voice
+
+    for voice in root.findall(".//voice"):
+
+        if voice.text is None:
+
+            voice.text="1"
 
 
 
     # 修正 duration
+
     for d in root.findall(".//duration"):
 
         try:
 
-            value = int(d.text)
+            if int(d.text)<=0:
 
-            if value <= 0:
-                d.text = "1"
+                d.text="1"
 
         except:
 
-            d.text = "1"
-
-
-
-    # 修正拍號
-    for beats in root.findall(".//beats"):
-
-        if beats.text not in [
-            "2",
-            "3",
-            "4",
-            "6"
-        ]:
-            beats.text="4"
-
-
-
-    for beat in root.findall(".//beat-type"):
-
-        if beat.text not in [
-            "2",
-            "4",
-            "8"
-        ]:
-            beat.text="4"
+            d.text="1"
 
 
 
@@ -111,15 +114,8 @@ def clean_musicxml(input_file, output_file):
     print("clean完成")
 
 
+
 if __name__=="__main__":
-
-    if len(sys.argv)<3:
-
-        print(
-            "python clean_musicxml.py input.musicxml output.musicxml"
-        )
-
-        exit()
 
 
     clean_musicxml(
