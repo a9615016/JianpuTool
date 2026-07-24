0b93b15b978551149a5dcbfd5f9a4bc215bb726a
a9615016 <a9615016@gmail.com>
2026-07-24 15:56:15 +0800

    Update rebuild_musicxml.py

--- rebuild_musicxml.py
+++ rebuild_musicxml.py
@@ -1,80 +1,44 @@
 import sys
-import music21
+from music21 import converter, stream, meter
 
 
 print("REBUILD VERSION 20260724 V4")
 
 
-def fix_duration(obj):
-
-    try:
-
-        ql = obj.duration.quarterLength
-
-
-        # jianpu_ly 支援的最大值
-        allowed = [
-            0.5,
-            0.75,
-            1,
-            1.5,
-            2,
-            3,
-            4,
-            6,
-            8,
-            12
-        ]
-
-
-        # 超長音符拆開
-        if ql > 12:
-
-            obj.duration.quarterLength = 12
-
-
-        # 不可表示的小數
-        elif ql not in allowed:
-
-            # 四捨五入到最近拍值
-            nearest = min(
-                allowed,
-                key=lambda x:abs(x-ql)
-            )
-
-            obj.duration.quarterLength = nearest
-
-
-    except Exception:
-
-        pass
-
-
-
 def rebuild(input_file, output_file):
-
 
     print("開始重建 MusicXML")
 
-
-    score = music21.converter.parse(
-        input_file
-    )
+    score = converter.parse(input_file)
 
 
     for part in score.parts:
 
-        for element in part.recurse():
+        # 強制 4/4
+        part.insert(
+            0,
+            meter.TimeSignature("4/4")
+        )
 
-            if isinstance(
-                element,
-                (
-                    music21.note.Note,
-                    music21.note.Rest
-                )
-            ):
 
-                fix_duration(element)
+        for measure in part.getElementsByClass(
+            stream.Measure
+        ):
+
+            # 修正怪 duration
+            for element in measure.notesAndRests:
+
+                ql = element.duration.quarterLength
+
+
+                # 不合法長度修正
+                if ql > 4:
+
+                    element.duration.quarterLength = 4
+
+
+                # 強制重新計算 type
+                element.duration.linked = True
 
 
 
