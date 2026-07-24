7317f5b406de30162492e2c7b3b4bcc1b1504617
a9615016 <a9615016@gmail.com>
2026-07-24 12:28:51 +0800

    Update clean_musicxml.py

--- clean_musicxml.py
+++ clean_musicxml.py
@@ -6,44 +6,55 @@ def clean_musicxml(input_file, output_file):
 
     print("開始 clean MusicXML")
 
+
     tree = ET.parse(input_file)
     root = tree.getroot()
 
 
-    # namespace
-    ns = {
-        "m": "http://www.musicxml.org/ns/musicxml"
-    }
+    ns = "{http://www.musicxml.org/ns/musicxml}"
 
 
-    # 移除所有 pickup
-    for measure in root.findall(".//m:measure", ns):
+    # 1. 強制刪除 pickup 標記
+    for measure in root.findall(".//" + ns + "measure"):
 
-        attrs = measure.find("m:attributes", ns)
-
-        if attrs is not None:
-
-            # 改拍號
-            time = attrs.find("m:time", ns)
-
-            if time is not None:
-
-                beats = time.find("m:beats", ns)
-                beat_type = time.find("m:beat-type", ns)
-
-                if beats is not None:
-                    beats.text = "4"
-
-                if beat_type is not None:
-                    beat_type.text = "4"
+        if measure.attrib.get("implicit") == "yes":
+            measure.attrib.pop("implicit")
 
 
-        # 移除 pickup 標記
-        for elem in measure.findall(".//m:forward", ns):
-            measure.remove(elem)
+    # 2. 第一小節移除 forward / backup
+    measures = root.findall(".//" + ns + "measure")
+
+    if len(measures) > 0:
+
+        first = measures[0]
+
+        for x in list(first):
+
+            if x.tag in [
+                ns+"forward",
+                ns+"backup"
+            ]:
+                first.remove(x)
 
 
-    # 修正 encoding
+
+    # 3. 所有拍號固定 4/4
+
+    for time in root.findall(".//"+ns+"time"):
+
+        beats = time.find(ns+"beats")
+        beat_type = time.find(ns+"beat-type")
+
+        if beats is not None:
+            beats.text="4"
+
+        if beat_type is not None:
+            beat_type.text="4"
+
+
+
+    # 4. 修正 XML encoding
+
     tree.write(
         output_file,
         encoding="utf-8",
@@ -56,12 +67,11 @@ def clean_musicxml(input_file, output_file):
 
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
 
-    if len(sys.argv) < 3:
+    if len(sys.argv)<3:
         print(
-            "使用方式:\n"
-            "python clean_musicxml.py input.musicxml output.musicxml"
+        "python clean_musicxml.py input.musicxml output.musicxml"
         )
         exit()
 
