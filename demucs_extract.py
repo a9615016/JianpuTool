3fdeee80e802d022344485827975f20ea4ef53b7
a9615016 <a9615016@gmail.com>
2026-07-25 14:43:12 +0800

    Update demucs_extract.py

--- demucs_extract.py
+++ demucs_extract.py
@@ -1,25 +1,58 @@
 import subprocess
 import sys
 import os
+import shutil
 
 
 def extract_vocal(mp3, output):
 
+    print("開始 Demucs")
+    print("輸入:", mp3)
+
+
+    workdir = os.path.dirname(output)
+
+    separated = os.path.join(
+        workdir,
+        "separated"
+    )
+
+
     cmd = [
         "demucs",
+        "-n",
+        "htdemucs",
         "--two-stems=vocals",
         "-o",
-        "separated",
+        separated,
         mp3
     ]
 
-    subprocess.run(cmd, check=True)
+
+    result = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
+    )
 
 
-    name = os.path.splitext(os.path.basename(mp3))[0]
+    print(result.stdout)
+
+
+    if result.returncode != 0:
+        raise Exception(
+            result.stdout
+        )
+
+
+    name = os.path.splitext(
+        os.path.basename(mp3)
+    )[0]
+
 
     vocals = os.path.join(
-        "separated",
+        separated,
         "htdemucs",
         name,
         "vocals.wav"
@@ -27,10 +60,22 @@ def extract_vocal(mp3, output):
 
 
     if not os.path.exists(vocals):
-        raise Exception("Demucs 沒產生 vocals.wav")
+        raise Exception(
+            "Demucs 沒產生 vocals.wav\n"
+            + vocals
+        )
 
 
-    os.rename(vocals, output)
+    shutil.move(
+        vocals,
+        output
+    )
+
+
+    print(
+        "完成:",
+        output
+    )
 
 
 if __name__ == "__main__":
