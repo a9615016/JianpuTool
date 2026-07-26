5c27a2e2dc80e136a23b8c76357665425d9f06c4
a9615016 <a9615016@gmail.com>
2026-07-26 17:05:57 +0800

    Update basic_pitch_convert.py

--- basic_pitch_convert.py
+++ basic_pitch_convert.py
@@ -1,13 +1,12 @@
 import sys
 import os
 
-from basic_pitch.inference import predict
-from basic_pitch import ICASSP_2022_MODEL_PATH
+from basic_pitch.inference import predict_and_save
 
 
 if len(sys.argv) < 3:
-    print("usage: python basic_pitch_convert.py input.mp3 output.mid")
-    sys.exit(1)
+    print("Usage: python basic_pitch_convert.py input.mp3 output.mid")
+    exit(1)
 
 
 input_audio = sys.argv[1]
@@ -18,18 +17,31 @@ print("BasicPitch input:")
 print(input_audio)
 
 
-print("Predicting MIDI...")
+output_dir = os.path.dirname(output_midi)
+
+os.makedirs(output_dir, exist_ok=True)
 
 
-_, midi_data, _ = predict(
-    input_audio,
-    model_or_model_path=ICASSP_2022_MODEL_PATH
+predict_and_save(
+    [input_audio],
+    output_dir,
+    save_midi=True,
+    sonify_midi=False,
+    save_model_outputs=False,
+    save_notes=False
 )
 
 
-print("Writing MIDI:")
+# BasicPitch 會自己產生名字
+# 找 midi
+for f in os.listdir(output_dir):
+    if f.endswith(".mid"):
+        src = os.path.join(output_dir,f)
 
-midi_data.write(output_midi)
+        if src != output_midi:
+            os.rename(src, output_midi)
+
+        break
 
 
 print("MIDI完成:")
