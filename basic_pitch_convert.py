6e1a21abc64bf5fecfbb2900b2388b467d20b6c5
a9615016 <a9615016@gmail.com>
2026-07-26 17:12:24 +0800

    Update basic_pitch_convert.py

--- basic_pitch_convert.py
+++ basic_pitch_convert.py
@@ -6,6 +6,8 @@ from basic_pitch.inference import (
     Model
 )
 
+from basic_pitch import ICASSP_2022_MODEL_PATH
+
 
 if len(sys.argv) < 3:
     print("Usage: python basic_pitch_convert.py input.mp3 output.mid")
@@ -25,8 +27,12 @@ output_dir = os.path.dirname(output_midi)
 os.makedirs(output_dir, exist_ok=True)
 
 
-# 載入 BasicPitch 模型
-model = Model()
+print("Loading BasicPitch model")
+
+model = Model(ICASSP_2022_MODEL_PATH)
+
+
+print("Predicting MIDI...")
 
 
 predict_and_save(
@@ -40,17 +46,23 @@ predict_and_save(
 )
 
 
-# 找產生的 midi
+# 找 MIDI
+found = False
+
 for file in os.listdir(output_dir):
 
     if file.endswith(".mid"):
 
         src = os.path.join(output_dir, file)
 
-        if src != output_midi:
-            os.rename(src, output_midi)
+        os.rename(src, output_midi)
 
+        found = True
         break
+
+
+if not found:
+    raise Exception("BasicPitch did not generate MIDI")
 
 
 print("MIDI完成:")
