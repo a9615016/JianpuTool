b2965fd7f76b66bb606716f92725b864caddba2e
a9615016 <a9615016@gmail.com>
2026-07-26 16:48:30 +0800

    Update basic_pitch_convert.py

--- basic_pitch_convert.py
+++ basic_pitch_convert.py
@@ -1,7 +1,11 @@
 import sys
 import os
 
-from basic_pitch.inference import predict_and_save
+from basic_pitch.inference import (
+    predict,
+    Model
+)
+
 from basic_pitch import ICASSP_2022_MODEL_PATH
 
 
@@ -13,38 +17,28 @@ if len(sys.argv) < 3:
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-output_dir = os.path.dirname(output_file)
-
 
 print("BasicPitch input:")
 print(input_file)
 
 
-predict_and_save(
-    [input_file],          # audio_paths
-    output_dir,            # output_directory
-    True,                  # save_midi
-    False,                 # sonify_midi
-    False,                 # save_model_outputs
-    True,                  # save_notes
-    ICASSP_2022_MODEL_PATH # model_or_model_path
+# 載入模型
+model = Model(
+    ICASSP_2022_MODEL_PATH
 )
 
 
-# BasicPitch 預設輸出
-generated = os.path.join(
-    output_dir,
-    os.path.splitext(
-        os.path.basename(input_file)
-    )[0] + ".mid"
+# 推論
+model_output, midi_data, note_events = predict(
+    input_file,
+    model
 )
 
 
-if os.path.exists(generated):
-
-    os.rename(
-        generated,
-        output_file
+# 輸出 MIDI
+with open(output_file, "wb") as f:
+    f.write(
+        midi_data.get_bytes()
     )
 
 
