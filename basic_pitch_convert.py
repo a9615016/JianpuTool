ba97ee5f4f93954e1c6211f2e25d61b7a3b38e88
a9615016 <a9615016@gmail.com>
2026-07-26 16:31:25 +0800

    Update basic_pitch_convert.py

--- basic_pitch_convert.py
+++ basic_pitch_convert.py
@@ -2,22 +2,52 @@ import sys
 import os
 
 from basic_pitch.inference import predict_and_save
+from basic_pitch import ICASSP_2022_MODEL_PATH
 
 
-if __name__ == "__main__":
+if len(sys.argv) < 3:
+    print("usage: python basic_pitch_convert.py input.mp3 output.mid")
+    exit(1)
 
-    input_file = sys.argv[1]
-    output_dir = sys.argv[2]
 
-    print("開始 BasicPitch")
-    print("輸入:", input_file)
+input_audio = sys.argv[1]
+output_mid = sys.argv[2]
 
-    predict_and_save(
-        [input_file],
-        output_dir,
-        True,
-        True,
-        False
+
+print("開始 BasicPitch")
+print("輸入:", input_audio)
+
+
+output_dir = os.path.dirname(output_mid)
+
+
+predict_and_save(
+    [input_audio],
+    output_dir,
+    True,                  # save_midi
+    True,                  # sonify_midi
+    True,                  # save_model_outputs
+    True,                  # save_notes
+    ICASSP_2022_MODEL_PATH
+)
+
+
+# BasicPitch輸出的檔名
+generated = os.path.join(
+    output_dir,
+    os.path.splitext(
+        os.path.basename(input_audio)
+    )[0] + ".mid"
+)
+
+
+if os.path.exists(generated):
+
+    os.rename(
+        generated,
+        output_mid
     )
 
-    print("BasicPitch完成")
+
+print("MIDI完成:")
+print(output_mid)
