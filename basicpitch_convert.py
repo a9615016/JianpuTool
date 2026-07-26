610cb972e6bf0de0ef3b5ffd9c406d2c9fb38007
a9615016 <a9615016@gmail.com>
2026-07-26 09:31:06 +0800

    Update basicpitch_convert.py

--- basicpitch_convert.py
+++ basicpitch_convert.py
@@ -1,26 +1,24 @@
 import sys
-import os
 
 from basic_pitch.inference import predict
 from basic_pitch import ICASSP_2022_MODEL_PATH
-from basic_pitch.utilities import note_events_to_midi
 
 
 input_audio = sys.argv[1]
 output_midi = sys.argv[2]
 
 
-print("BasicPitch input:", input_audio)
+print("開始 BasicPitch")
+print("輸入:", input_audio)
 
 
-model_output, midi_data, note_events = predict(
+_, midi_data, note_events = predict(
     input_audio,
     ICASSP_2022_MODEL_PATH
 )
 
 
-with open(output_midi, "wb") as f:
-    midi_data.write(f)
+midi_data.write(output_midi)
 
 
-print("MIDI完成:", output_midi)
+print("MIDI完成")
