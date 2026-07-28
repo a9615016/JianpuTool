f92c664c32ada25baea83b28696dd2c7a71aca22
a9615016 <a9615016@gmail.com>
2026-07-28 16:28:27 +0800

    Update basicpitch_convert.py

--- basicpitch_convert.py
+++ basicpitch_convert.py
@@ -1,95 +1,27 @@
-import sys
-import os
-import logging
-
-from basic_pitch.inference import predict_and_save
+from basic_pitch.inference import predict
 from basic_pitch import ICASSP_2022_MODEL_PATH
+from basic_pitch.note_creation import note_events_to_midi
+import sys
 
 
-logging.basicConfig(level=logging.INFO)
+input_audio=sys.argv[1]
+output_mid=sys.argv[2]
 
 
-def convert_audio_to_midi(input_audio, output_midi):
-
-    print("====================")
-    print("開始 BasicPitch")
-    print("輸入:", input_audio)
-
-    if not os.path.exists(input_audio):
-        raise FileNotFoundError(input_audio)
+model_path=ICASSP_2022_MODEL_PATH
 
 
-    output_dir = os.path.dirname(output_midi)
-
-    if output_dir and not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-
-    print("Predicting MIDI...")
+model_output = predict(
+    input_audio,
+    model_path
+)
 
 
-    predict_and_save(
-        [input_audio],
-        output_directory=output_dir,
-        save_midi=True,
-        sonify_midi=False,
-        save_model_outputs=False,
-        model_or_model_path=ICASSP_2022_MODEL_PATH
-    )
+note_events = model_output[0]
 
+note_events_to_midi(
+    note_events,
+    output_mid
+)
 
-    # BasicPitch 預設名稱
-    generated = os.path.join(
-        output_dir,
-        os.path.splitext(
-            os.path.basename(input_audio)
-        )[0] + ".mid"
-    )
-
-
-    if os.path.exists(generated):
-
-        os.rename(
-            generated,
-            output_midi
-        )
-
-    else:
-        # 某些版本輸出 midi
-        mids = [
-            f for f in os.listdir(output_dir)
-            if f.endswith(".mid")
-        ]
-
-        if len(mids) == 0:
-            raise Exception(
-                "BasicPitch 沒有產生 MIDI"
-            )
-
-        os.rename(
-            os.path.join(output_dir, mids[0]),
-            output_midi
-        )
-
-
-    print("MIDI完成:", output_midi)
-
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv) < 3:
-        print(
-            "用法: python basicpitch_convert.py input.mp3 output.mid"
-        )
-        sys.exit(1)
-
-
-    input_audio = sys.argv[1]
-    output_midi = sys.argv[2]
-
-
-    convert_audio_to_midi(
-        input_audio,
-        output_midi
-    )
+print("MIDI created:", output_mid)
