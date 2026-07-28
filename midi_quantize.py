05403d0a1ff259bbaf4fea713c646c80f68dc6a6
a9615016 <a9615016@gmail.com>
2026-07-28 14:53:23 +0800

    Update midi_quantize.py

--- midi_quantize.py
+++ midi_quantize.py
@@ -1,87 +1,79 @@
-from mido import MidiFile, MidiTrack
+from music21 import converter
 import sys
-import os
-
-VERSION = "MIDI QUANTIZE V2"
 
 
-def quantize_tick(tick, grid):
-    return int(round(tick / grid) * grid)
+print("================")
+print("MIDI QUANTIZE V1")
+print("================")
 
 
-def clean_midi(input_file, output_file):
+if len(sys.argv) < 3:
+    print(
+        "python midi_quantize.py input.mid output.mid"
+    )
+    sys.exit()
 
-    print(VERSION)
-    print("input:", input_file)
 
-    mid = MidiFile(input_file)
+inp = sys.argv[1]
+out = sys.argv[2]
 
-    grid = max(1, mid.ticks_per_beat // 4)   # 16分音符
 
-    print("ticks_per_beat:", mid.ticks_per_beat)
-    print("grid:", grid)
+score = converter.parse(inp)
 
-    new_mid = MidiFile(
-        ticks_per_beat=mid.ticks_per_beat
+
+print("quantize notes")
+
+
+# 1/16拍量化
+grid = 0.25
+
+
+for n in score.recurse().notes:
+
+    offset = float(n.offset)
+
+    new_offset = round(
+        offset / grid
+    ) * grid
+
+
+    n.offset = new_offset
+
+
+
+    dur = float(
+        n.duration.quarterLength
     )
 
-    for track in mid.tracks:
 
-        abs_time = 0
-        events = []
-
-        # 收集事件（絕對時間）
-        for msg in track:
-
-            abs_time += msg.time
-
-            if msg.type in ("note_on", "note_off"):
-                events.append(
-                    (
-                        quantize_tick(abs_time, grid),
-                        msg
-                    )
-                )
-
-        # 按時間排序
-        events.sort(key=lambda x: x[0])
-
-        new_track = MidiTrack()
-
-        last_time = 0
-
-        for abs_tick, msg in events:
-
-            delta = abs_tick - last_time
-
-            if delta < 0:
-                delta = 0
-
-            new_track.append(
-                msg.copy(time=delta)
-            )
-
-            last_time = abs_tick
-
-        new_mid.tracks.append(new_track)
-
-    new_mid.save(output_file)
-
-    print("done:")
-    print(output_file)
+    new_dur = round(
+        dur / grid
+    ) * grid
 
 
-if __name__ == "__main__":
+    if new_dur <= 0:
+        new_dur = grid
 
-    if len(sys.argv) < 2:
-        print("python midi_quantize.py input.mid output.mid")
-        sys.exit(1)
 
-    inp = sys.argv[1]
+    n.duration.quarterLength = new_dur
 
-    if len(sys.argv) >= 3:
-        out = sys.argv[2]
-    else:
-        out = os.path.splitext(inp)[0] + "_clean.mid"
 
-    clean_midi(inp, out)
+
+print("make measures")
+
+
+score.makeMeasures(
+    inPlace=True
+)
+
+
+print("write")
+
+
+score.write(
+    "midi",
+    fp=out
+)
+
+
+print("DONE")
