c9da3587edf742b9ced610548c8c4e716249c2e7
a9615016 <a9615016@gmail.com>
2026-08-02 17:57:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,55 @@
 import streamlit as st
+import os
+import tempfile
+
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
+
 
 st.title("🎵 JianpuTool")
+st.write("MP3 → MIDI → 簡譜")
 
-st.write("MP3 → 簡譜")
 
 uploaded = st.file_uploader(
-    "選擇MP3",
-    type=["mp3","wav"]
+    "上傳 MP3/WAV",
+    type=["mp3", "wav"]
 )
 
+
 if uploaded:
-    st.success("收到")
+
+    input_file = tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=".mp3"
+    )
+
+    input_file.write(uploaded.read())
+    input_file.close()
+
+
+    st.info("開始 BasicPitch 分析...")
+
+
+    try:
+        model_output, midi_data, note_events = predict(
+            input_file.name,
+            ICASSP_2022_MODEL_PATH
+        )
+
+
+        midi_path = "output.mid"
+
+        midi_data.write(midi_path)
+
+
+        st.success("✅ MIDI 產生成功")
+
+        st.download_button(
+            "下載 MIDI",
+            open(midi_path,"rb"),
+            file_name="output.mid"
+        )
+
+
+    except Exception as e:
+        st.error(e)
