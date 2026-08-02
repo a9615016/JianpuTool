9412a8d5ca2f7a04deed9041a52b09e9a877dba7
a9615016 <a9615016@gmail.com>
2026-08-02 17:46:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.title("JianpuTool")
+st.title("🎵 JianpuTool")
 
-st.write("MP3 → MIDI → MusicXML → 簡譜 PDF")
+st.write("MP3 → 簡譜")
 
 uploaded = st.file_uploader(
-    "上傳 MP3",
+    "選擇MP3",
     type=["mp3","wav"]
 )
 
 if uploaded:
-    st.success("收到音檔：" + uploaded.name)
+    st.success("收到")
