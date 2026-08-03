722c29d16117f270e83032d1403c2acff19d3c6a
a9615016 <a9615016@gmail.com>
2026-08-03 12:31:42 +0800

    Update bar_split_fix.py

--- bar_split_fix.py
+++ bar_split_fix.py
@@ -1,185 +1,78 @@
 import sys
-from music21 import converter, stream, note, chord, meter, duration
+from music21 import converter, stream, note, chord, meter
 
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 
+print("Loading MusicXML")
+
 score = converter.parse(input_file)
 
 
-new_score = stream.Score()
+print("Fix measures")
 
 
+# 強制 4/4
+for part in score.parts:
 
-def make_duration(obj, ql):
+    measures = part.getElementsByClass(stream.Measure)
 
-    obj.duration = duration.Duration(
-        ql
+    for m in measures:
+
+        if m.timeSignature is None:
+            m.insert(
+                0,
+                meter.TimeSignature("4/4")
+            )
+
+
+# 修復 duration
+for element in score.recurse():
+
+    if isinstance(element, (note.Note, note.Rest, chord.Chord)):
+
+        ql = element.duration.quarterLength
+
+        if ql <= 0:
+            ql = 1.0
+
+        element.duration.quarterLength = ql
+
+        # 關鍵
+        element.duration._type = None
+
+        element.duration.type = element.duration.type
+
+
+print("Make measures")
+
+
+try:
+
+    score.makeMeasures(
+        inPlace=True
     )
 
-    # 強制產生 type
-    obj.duration.quarterLength = ql
-
-    return obj
-
-
-
-for old_part in score.parts:
-
-
-    new_part = stream.Part()
-
-
-    new_part.insert(
-        0,
-        meter.TimeSignature("4/4")
-    )
-
-
-    measure_no = 1
-
-    measure = stream.Measure(
-        number=measure_no
-    )
-
-
-    used = 0.0
-
-
-
-    for el in old_part.flatten().notesAndRests:
-
-
-        remaining = float(
-            el.duration.quarterLength
-        )
-
-
-        while remaining > 0:
-
-
-            space = 4.0 - used
-
-
-            take = min(
-                space,
-                remaining
-            )
-
-
-            if isinstance(el, note.Note):
-
-                obj = note.Note(
-                    el.pitch
-                )
-
-
-            elif isinstance(el, chord.Chord):
-
-                obj = chord.Chord(
-                    el.pitches
-                )
-
-
-            else:
-
-                obj = note.Rest()
-
-
-
-            make_duration(
-                obj,
-                take
-            )
-
-
-            measure.append(
-                obj
-            )
-
-
-            used += take
-
-            remaining -= take
-
-
-
-            if used >= 4.0 - 0.0001:
-
-
-                new_part.append(
-                    measure
-                )
-
-
-                measure_no += 1
-
-
-                measure = stream.Measure(
-                    number=measure_no
-                )
-
-
-                used = 0
-
-
-
-    if len(measure.notesAndRests):
-
-
-        if used < 4:
-
-            r = note.Rest()
-
-            make_duration(
-                r,
-                4-used
-            )
-
-            measure.append(r)
-
-
-
-        new_part.append(
-            measure
-        )
-
-
-    new_score.append(
-        new_part
+except Exception as e:
+
+    print(
+        "makeMeasures skip:",
+        e
     )
 
 
-
-# 重新排譜
-
-new_score.makeNotation(
-    inPlace=True
-)
+print("Writing")
 
 
-# 再檢查一次 duration
-
-for p in new_score.parts:
-
-    for n in p.flatten().notesAndRests:
-
-        if not n.duration.type:
-
-            n.duration = duration.Duration(
-                n.duration.quarterLength
-            )
-
-
-
-new_score.write(
+score.write(
     "musicxml",
     fp=output_file
 )
 
 
 print(
-    "BAR SPLIT OK"
+    "DONE",
+    output_file
 )
