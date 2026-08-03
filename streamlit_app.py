1f09fd6ed22299a938ab756439df58188cc93187
a9615016 <a9615016@gmail.com>
2026-08-03 10:34:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
+import os
+import uuid
+import subprocess
 
 
 st.set_page_config(
     page_title="JianpuTool",
     page_icon="🎵"
 )
-
 
 st.title("🎵 JianpuTool")
 
@@ -14,19 +16,77 @@ st.write(
 )
 
 
-uploaded_file = st.file_uploader(
+UPLOAD_DIR = "uploads"
+OUTPUT_DIR = "outputs"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+mp3 = st.file_uploader(
     "上傳 MP3",
     type=["mp3"]
 )
 
 
-if uploaded_file:
+if mp3:
 
-    st.success(
-        "MP3 上傳完成"
+    job = str(uuid.uuid4())
+
+    mp3_path = os.path.join(
+        UPLOAD_DIR,
+        job + ".mp3"
     )
+
+
+    with open(mp3_path, "wb") as f:
+        f.write(mp3.getbuffer())
+
+
+    st.success("MP3 上傳完成")
 
     st.write(
         "檔案名稱：",
-        uploaded_file.name
+        mp3.name
     )
+
+
+    if st.button("開始轉換"):
+
+        midi_path = os.path.join(
+            OUTPUT_DIR,
+            job + ".mid"
+        )
+
+        st.info(
+            "BasicPitch 分析音樂..."
+        )
+
+
+        result = subprocess.run(
+            [
+                "python",
+                "basicpitch_convert.py",
+                mp3_path,
+                midi_path
+            ],
+            capture_output=True,
+            text=True
+        )
+
+
+        if result.returncode != 0:
+
+            st.error(
+                "BasicPitch 失敗"
+            )
+
+            st.code(
+                result.stderr
+            )
+
+        else:
+
+            st.success(
+                "MIDI 產生成功"
+            )
