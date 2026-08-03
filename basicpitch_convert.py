176d446973ac7306a67e0c6f868423217f0e6050
a9615016 <a9615016@gmail.com>
2026-08-03 09:29:36 +0800

    Update basicpitch_convert.py

--- basicpitch_convert.py
+++ basicpitch_convert.py
@@ -3,64 +3,32 @@ import os
 
 from basic_pitch.inference import predict
 from basic_pitch import ICASSP_2022_MODEL_PATH
-from basic_pitch.note_creation import model_output_to_notes
-from basic_pitch import inference
-
-
-print("開始 BasicPitch")
 
 
 if len(sys.argv) < 3:
     print(
-        "使用方法: python basicpitch_convert.py input.mp3 output.mid"
+        "用法: python basicpitch_convert.py input.mp3 output.mid"
     )
     sys.exit(1)
 
 
-
-INPUT = os.path.abspath(sys.argv[1])
-OUTPUT = os.path.abspath(sys.argv[2])
-
-
-print("輸入:")
-print(INPUT)
-
-print("輸出:")
-print(OUTPUT)
+input_audio = sys.argv[1]
+output_midi = sys.argv[2]
 
 
-
-# ==========================
-# BasicPitch Predict
-# ==========================
-
-print("Predicting MIDI for", INPUT)
+print("輸入:", input_audio)
+print("輸出:", output_midi)
 
 
 model_output, midi_data, note_events = predict(
-    INPUT,
+    input_audio,
     ICASSP_2022_MODEL_PATH
 )
 
 
-print("note events:", len(note_events))
-
-
-
-# ==========================
-# 儲存 MIDI
-# ==========================
-
-os.makedirs(
-    os.path.dirname(OUTPUT),
-    exist_ok=True
-)
-
-
 midi_data.write(
-    OUTPUT
+    output_midi
 )
 
 
-print("DONE")
-print(OUTPUT)
+print("完成 MIDI:", output_midi)
