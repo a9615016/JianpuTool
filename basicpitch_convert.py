492c4e7afadffbd992367e842db669de49da565c
a9615016 <a9615016@gmail.com>
2026-08-03 09:36:31 +0800

    Update basicpitch_convert.py

--- basicpitch_convert.py
+++ basicpitch_convert.py
@@ -1,34 +1,35 @@
 import sys
-import os
+import traceback
 
-from basic_pitch.inference import predict
-from basic_pitch import ICASSP_2022_MODEL_PATH
+try:
+    from basic_pitch.inference import predict
+    from basic_pitch import ICASSP_2022_MODEL_PATH
+
+    print("BasicPitch import OK")
 
 
-if len(sys.argv) < 3:
-    print(
-        "用法: python basicpitch_convert.py input.mp3 output.mid"
+    input_audio = sys.argv[1]
+    output_midi = sys.argv[2]
+
+
+    print("INPUT:", input_audio)
+
+
+    model_output, midi_data, note_events = predict(
+        input_audio,
+        ICASSP_2022_MODEL_PATH
     )
+
+
+    midi_data.write(
+        output_midi
+    )
+
+
+    print("DONE:", output_midi)
+
+
+except Exception:
+
+    traceback.print_exc()
     sys.exit(1)
-
-
-input_audio = sys.argv[1]
-output_midi = sys.argv[2]
-
-
-print("輸入:", input_audio)
-print("輸出:", output_midi)
-
-
-model_output, midi_data, note_events = predict(
-    input_audio,
-    ICASSP_2022_MODEL_PATH
-)
-
-
-midi_data.write(
-    output_midi
-)
-
-
-print("完成 MIDI:", output_midi)
